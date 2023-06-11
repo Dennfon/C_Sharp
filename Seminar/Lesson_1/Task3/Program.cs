e8ebc786d5dcd50adbc96b15657c80382327f416
Denis <deni-alfimo@yandex.ru>
2023-06-11 18:54:08 +0300

    add Task3

---
+++ Seminar/Lesson_1/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Напишите целое положительное число");
+string imput = Console.ReadLine();
+int number1 = Convert.ToInt32(imput);
+int number2 = -number1;
+while (number2 <= number1)
+{
+    Console.Write(number2);
+    number2++;
+}
