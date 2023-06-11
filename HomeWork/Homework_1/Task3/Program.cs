ce5fab3d32dd749bfa131d3696cf801f9ae7bbba
Denis <deni-alfimo@yandex.ru>
2023-06-11 20:31:51 +0300

    add Task3 end

---
+++ HomeWork/Homework_1/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите целое число => ");
+string one = Console.ReadLine();
+int number1 = Convert.ToInt32(one);
+
+int count = 1;
+
+while (count <= number1)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+}
