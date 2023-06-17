008321beb3df69cda524a8bdcb72a16c5342786a
Denis <deni-alfimo@yandex.ru>
2023-06-17 18:12:26 +0300

    add omework 3

---
+++ HomeWork/Homework_3/Task0/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача 1
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int GetLength(int num)
+{
+    int temp = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        temp++;
+    }
+    return temp;
+}
+
+bool IsPalindrome(int num)
+{
+
+    int Lenght = GetLength(num);
+    int[] array = new int[Lenght];
+    for (int i = Lenght - 1; i >= 0; i--)
+    {
+        array[i] = num % 10;
+        num /= 10;
+    }
+    for (int i = 0; i < Lenght / 2; i++)
+    {
+        if (array[i] != array[Lenght - i - 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+
+
+int number = ReadInt("Введите целое положительное число >");
+System.Console.WriteLine(IsPalindrome(number));
+
---
+++ HomeWork/Homework_3/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine($"{message}");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+double Length(int x, int y, int z)
+{
+    double temp = x * x + y * y + z * z;
+    return Math.Sqrt(temp);
+}
+
+int x1 = ReadInt("Введите координату X первой точки > ");
+int y1 = ReadInt("Введите координату Y второй точки > ");
+int z1 = ReadInt("Введите координату Z первой точки > ");
+int x2 = ReadInt("Введите координату X первой точки > ");
+int y2 = ReadInt("Введите координату Y второй точки > ");
+int z2 = ReadInt("Введите координату Z второй точки > ");
+
+double result = Length(x2 - x1, y2 - y1, z2 - z1);
+System.Console.WriteLine($"Расстояние между двумя точками равно:  {result}");
+
+
+
