c0d839d7cb0cc1f07533e461a1d6db95aaddc93f
Denis <deni-alfimo@yandex.ru>
2023-06-20 21:50:24 +0300

    add Lesson_4

---
+++ Exampels/Example016_ArraySortBigToSmall/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void ShowArray(int[] arr)
+{
+    int count = arr.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+ShowArray(arr);
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+SelectionSort(arr);
+ShowArray(arr);
---
+++ Seminar/Lesson_4/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedStr = Console.ReadLine();
+    int value;
+    if (int.TryParse(inputedStr, out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Извините, но вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int number = Prompt("Введите число");
+
+int Faktorial(int number)
+{
+    int count = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        count *= i;
+    }
+
+    return count;
+}
+
+int sum = Faktorial(number);
+System.Console.WriteLine(sum);
