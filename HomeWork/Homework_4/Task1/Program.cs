eb288bdfca583d0de7658ab96d0a40091f2ae734
Denis <deni-alfimo@yandex.ru>
2023-06-21 21:16:57 +0300

    add Homework_4

---
+++ HomeWork/Homework_4/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedStr = Console.ReadLine();
+    int value;
+    if (int.TryParse(inputedStr, out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Извините, но вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int SumOfNumber(int number)
+{
+    int sum = 0;
+    for (int i = 0; i < number; i++)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int number = Prompt("Введите число");
+int sum = SumOfNumber(number);
+
+System.Console.WriteLine(sum);
---
+++ HomeWork/Homework_4/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+//(*) В 3-й задаче Доп сложность. Ввод с консоли “введите количество элементов массива”, “Введите минимальный порог случайных значений”, “Введите максимальный порог случайных значений”
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} ");
+    string inputedStr = Console.ReadLine();
+    int value;
+    if (int.TryParse(inputedStr, out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Извините, но вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int[] SetIntroductoryVariables()
+{
+    int len = Prompt("Введите количество элементов массива > ");
+    int min = Prompt("Введите минимальный порог случайных значений > ");
+    int max = Prompt("Введите максимальный порог случайных значений > ");
+    return GenerateArray(len, min, max);
+}
+
+int[] GenerateArray(int len, int min, int max)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+
+void PrintArrey(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "\t");
+    }
+}
+
+
+
+int[] number = SetIntroductoryVariables();
+PrintArrey(number);
