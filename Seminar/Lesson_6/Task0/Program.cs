aafc286b90a307855bb5a6ac976a667559e26d3e
Denis nout <deni-alfimo@yandex.ru>
2023-06-27 21:55:32 +0300

    Added Lesson 6 Seminar

---
+++ Seminar/Lesson_6/Task0/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 1: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+//[1 2 3 4 5] -> [5 4 3 2 1]
+//[6 7 3 6] -> [6 3 7 6]
+
+int[] GenerateArray(int len, int min, int max)
+{
+    int[] answer = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(min, max + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write(item + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] ReversArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length / 2; i++)
+    {  // кортеж (array[i], array[array.Length - 1 - i]) = (array[array.Length - 1 - i], array[i]); 
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+
+    return array;
+}
+
+int[] array = GenerateArray(5, 100, 1000);
+PrintArray(array);
+int[] mass = ReversArray(array);
+PrintArray(mass);
---
+++ Seminar/Lesson_6/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+void Triangl(int a, int b, int c)
+{
+   
+    string message = "";
+    if (a < c + b && b < a + c && c < a + b)
+    {
+        message = "Yes";
+    }
+      else
+    {
+        message = "NO";
+    }
+
+    System.Console.WriteLine(message);
+
+}
+
+Triangl(3, 5, 4);
