ebf6931ca6e34eff0fc5ee5b5cc72c9eef192e6e
Denis nout <deni-alfimo@yandex.ru>
2023-06-30 22:22:53 +0300

    Added seminar 7

---
+++ Seminar/Lesson_7/Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 3: Задайте двумерный массив. 
+Найдите сумму элементов главной диагонали.
+*/
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray2D(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            System.Console.Write($"{numbers[i,j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] GenerateArray2D(int rows, int columns)
+{
+    int[,] numbers = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(-10, 10);
+        }
+    }
+    return numbers;
+}
+
+int DiagSum(int[,] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+     sum += numbers[i, i];
+    }
+    return sum;
+}
+
+int[,] newArray = GenerateArray2D(4,4);
+PrintArray2D(newArray);
+Console.WriteLine();
+int output = DiagSum(newArray);
+Console.WriteLine(output);
